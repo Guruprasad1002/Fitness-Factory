4f7afdbd32a8d325ba11d4ad1ad06d5b5ecd7526
Guruprasad HG <guruprasad.rao38@gmail.com>
2025-02-14 07:30:41 +0530

    Fitness Factory

---
+++ trainer/afooter.php
@@ -0,0 +1,8 @@
+<footer class="site-footer"  style="background-color:black">
+          <div class="text-center">
+              FITNESS MANTRA
+              <a href="" class="go-top">
+                  
+              </a>
+          </div>
+      </footer>
